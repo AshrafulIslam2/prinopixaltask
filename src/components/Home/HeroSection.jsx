81c0d42077cb7911f26309c8f8ec0302e908481a
ashcode <ashrafulislamsay7@gmail.com>
2024-03-21 17:55:49 +0600

    bug fixed

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <motion.div className=' p-[80px] z-30'
     initial={{scale:2.2 ,zIndex:"50"}}
     animate={{scale:1,zIndex:"30"}}
-    transition={{duration:1.5,ease:"linear"}}
+    transition={{duration:1,ease:"linear"}}
     style={{ width: "100%", height: "700px" ,overflow: "hidden"}} 
     >
       <video src='/home_desktop5_9DCE7153.mp4'   className="w-full h-full border  object-cover z-30"  muted
